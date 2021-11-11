080b997fec20faac8d0e9aa5e0eb93a4193d27bd
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-11 10:18:14 -0500

    swap variables

---
+++ playWIthVariables/swapVariables.js
@@ -0,0 +1,8 @@
+let number1 = 5;
+let number2 = 3;
+
+// TODO: type your code here (and nowhere else!)
+[number2, number1] = [number1, number2];
+
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
