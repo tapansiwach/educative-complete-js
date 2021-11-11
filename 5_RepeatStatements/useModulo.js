a8faa0df4c1690e4ca2dc23b7a3e2781b881d028
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-11 16:41:38 -0500

    count 10 odd numbers from a starting number

--- 5_RepeatStatements/useModulo.js
+++ 5_RepeatStatements/useModulo.js
@@ -1,10 +1,19 @@
-let num = 1;
-let oddCount = 0;
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-while (oddCount < 10) {
-  if (num % 2 === 1) {
-    console.log(`${num} is odd`);
-    oddCount++;
+readline.question("Enter the starting number: ", start => {
+  let num = start;
+  let oddCount = 0;
+
+  while (oddCount < 10) {
+    if (num % 2 === 1) {
+      console.log(`${num} is odd`);
+      oddCount++;
+    }
+    num++;
   }
-  num++;
-}
+
+  readline.close();
+});
