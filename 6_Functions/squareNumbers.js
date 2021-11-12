cf5d62b940ff3f69f0e6ccdc763d44a209438e15
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-12 06:56:07 -0500

    square numbers from 1 to 10

--- 6_Functions/squareNumbers.js
+++ 6_Functions/squareNumbers.js
@@ -5,10 +5,14 @@ function square1(x) {
 // Square the given number x
 const square2 = x => Math.pow(x, 2);
 
-console.log(square1(0));
-console.log(square1(2));
-console.log(square1(5));
+// console.log(square1(0));
+// console.log(square1(2));
+// console.log(square1(5));
 
-console.log(square2(0));
-console.log(square2(2));
-console.log(square2(5));
+// console.log(square2(0));
+// console.log(square2(2));
+// console.log(square2(5));
+
+for (let counter = 1; counter <= 10; counter++) {
+  console.log(square2(counter));
+}
