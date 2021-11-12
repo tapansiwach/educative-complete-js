acff38733a5101b521ef9af42332d43c0815f65f
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-12 07:34:33 -0500

    find circumference and area of a circle

---
+++ 6_Functions/circle.js
@@ -0,0 +1,15 @@
+function circumference(radius) {
+  return 2 * Math.PI * radius;
+}
+
+function area(radius) {
+  return Math.PI * radius * radius;
+}
+
+
+// checks
+console.log(circumference(2));
+console.log(circumference(12));
+
+console.log(area(20));
+console.log(area(4));
