d204a671e6b175a3ae0004c12e971f0998479c2c
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-12 06:30:07 -0500

    use prompt-sync for taking user input

--- 6_Functions/sayHello.js
+++ 6_Functions/sayHello.js
@@ -1,15 +1,21 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 const sayHello = (firstName, lastName) => {
   console.log(`Hello ${firstName} ${lastName}!`);
 }
 
-readline.question("Enter first name: ", fname => {
-  readline.question("Enter last name: ", lname => {
-    sayHello(fname, lname);
-    readline.close();
-  })
-})
+// readline.question("Enter first name: ", fname => {
+//   readline.question("Enter last name: ", lname => {
+//     sayHello(fname, lname);
+//     readline.close();
+//   })
+// })
+
+const prompt = require('prompt-sync')({ sigint: true });
+
+const fname = prompt("Enter first name: ");
+const lname = prompt("Enter last name: ");
+sayHello(fname, lname);
