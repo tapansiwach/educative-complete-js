d82a3ab0d386b875da3718c57d9a5450f5ac3e34
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-16 10:25:13 -0500

    create a RPG (role playing game) character

---
+++ 10_OOP/rpgCharacter.js
@@ -0,0 +1,36 @@
+class Character {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+    this.xp = 0; // XP is always zero for new characters
+  }
+  // Attack a target
+  attack(target) {
+    if (this.health > 0) {
+      const damage = this.strength;
+      console.log(
+        `${this.name} attacks ${target.name} and causes ${damage} damage points`
+      );
+      target.health -= damage;
+      if (target.health > 0) {
+        console.log(`${target.name} has ${target.health} health points left`);
+      } else {
+        target.health = 0;
+        const bonusXP = 10;
+        console.log(
+          `${this
+            .name} eliminated ${target.name} and wins ${bonusXP} experience points`
+        );
+        this.xp += bonusXP;
+      }
+    } else {
+      console.log(`${this.name} can't attack (they've been eliminated)`);
+    }
+  }
+  // Return the character description
+  describe() {
+    return `${this.name} has ${this.health} health points, ${this
+      .strength} as strength and ${this.xp} XP points`;
+  }
+}
