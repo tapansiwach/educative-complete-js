7f0481864832267aef71ed9132a39b67607acdd4
Tapan Siwach <siwach.tapan@gmail.com>
2021-11-24 10:11:22 -0500

    get titles of movies before 2000

---
+++ 11_FunctionlProgramming/olderMovies.js
@@ -0,0 +1,58 @@
+const movieList = [
+  {
+    title: "Batman",
+    year: 1989,
+    director: "Tim Burton",
+    imdbRating: 7.6
+  },
+  {
+    title: "Batman Returns",
+    year: 1992,
+    director: "Tim Burton",
+    imdbRating: 7.0
+  },
+  {
+    title: "Batman Forever",
+    year: 1995,
+    director: "Joel Schumacher",
+    imdbRating: 5.4
+  },
+  {
+    title: "Batman & Robin",
+    year: 1997,
+    director: "Joel Schumacher",
+    imdbRating: 3.7
+  },
+  {
+    title: "Batman Begins",
+    year: 2005,
+    director: "Christopher Nolan",
+    imdbRating: 8.3
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    director: "Christopher Nolan",
+    imdbRating: 9.0
+  },
+  {
+    title: "The Dark Knight Rises",
+    year: 2012,
+    director: "Christopher Nolan",
+    imdbRating: 8.5
+  }
+];
+
+/** a program that shows the titles of movies
+ * released before the year 2000
+ * expected output
+ * [ 'Batman', 'Batman Returns', 'Batman Forever', 'Batman & Robin' ]
+ */
+
+function olderMovies(movieList) {
+  return movieList
+    .filter(x => x.year < 2000)
+    .map(x => x.title);
+}
+
+console.log(olderMovies(movieList));
